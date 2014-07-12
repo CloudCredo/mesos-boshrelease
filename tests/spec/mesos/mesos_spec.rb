daa808c7a9ec8a0400515c85fad428830eb6ced1
Christopher Hedley <chris.hedley@opencredo.com>
2014-07-12 19:37:48 +0100

    Add mesos master spec

---
+++ tests/spec/mesos/mesos_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'yaml'
+
+describe 'Apache Mesos Bosh release' do
+
+  let(:mesos_url) { 'http://10.244.1.6:5050/' }
+
+  context 'Mesos UI is configured to run' do
+
+    it 'should should have an accessible homepage' do
+      status_code = Faraday.head(mesos_url).status
+      expect(status_code).to eq(200)
+    end
+
+    it 'the master is healthy' do
+      health_endpoint = "#{mesos_url}/master/health"
+      status_code = Faraday.head(health_endpoint).status
+      expect(status_code).to eq(200)
+    end
+
+    it 'should have the correct number of activated slaves' do
+      activeated_slaves_count = mesos_master_state_json.fetch('activated_slaves')
+      expect(activeated_slaves_count).to eq(expected_activated_slave_count)
+    end
+
+  end
+
+  private
+
+  def expected_activated_slave_count
+    job = active_bosh_manifest_yaml.fetch('jobs').find do |job|
+      job.fetch('name') == 'mesos-slave'
+    end
+    job.fetch('instances')
+  end
+
+end
